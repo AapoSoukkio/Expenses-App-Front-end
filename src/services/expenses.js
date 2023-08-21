592cf9c627f2c948968c60aaa9c591629ddad486
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-08-21 15:25:38 +0300

    Initial expences and reduser added

---
+++ src/services/expenses.js
@@ -0,0 +1,14 @@
+export const GetExpenses = async (dispatch) => {
+    try {
+        // api call
+        const expenses = [
+            {id: 1, description: 'Groceries', amount: 23.16},
+            {id: 2, description: 'Rent', amount: 345.00},
+            {id: 3, description: 'Gas', amount: 32.76},
+            {id: 4, description: 'Loan', amount: 120.37},
+        ];
+    } catch {
+        console.log("Error");
+        dispatch(setExpensesError());
+    }
+}
